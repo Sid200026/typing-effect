b6e165309d5ac90ebaf5c2067723e94732e1f207
Sid200026 <siddharthsingharoy@gmail.com>
2020-10-30 20:24:59 +0530

    feat(): Initialised Minimal React App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./container/App.jsx";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("sid200026__typing-effect")
+);
